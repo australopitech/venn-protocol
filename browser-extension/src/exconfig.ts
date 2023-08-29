f684369d9f8cc703a5e5192fb60f6c4367b1691c
pbfranceschin <pbfranceschin@gmail.com>
2023-08-29 10:01:11 -0300

    extension base chain config

--- browser-extension/src/exconfig.ts
+++ browser-extension/src/exconfig.ts
@@ -1,18 +1,27 @@
 import { EVMNetwork } from './pages/Background/types/network';
+import dotenv from 'dotenv';
+dotenv.config();
+
+const entryPointAddress = "0x5FF137D4b0FDCD49DcA30c7CF57E578a026d2789"; //stackup
+const networkID = 84531; // base goerli
+const network = "Base Goerli";
+const provider = process.env.BASE_GOERLI_PROVIDER;
+const bundler = process.env.BUNDLER_API;
+const factory = "0x684705F4A1C7cF696aD45e62Ea9E28e37a94b530"; // base goerli
 
 // eslint-disable-next-line import/no-anonymous-default-export
 export default {
   enablePasswordEncryption: false,
   showTransactionConfirmationScreen: true,
-  factory_address: '0x9406Cc6185a346906296840746125a0E44976454',
+  factory_address: factory,
   stateVersion: '0.1',
   network: {
-    chainID: '11155111',
+    chainID: String(networkID),
     family: 'EVM',
-    name: 'Sepolia',
-    provider: 'https://sepolia.infura.io/v3/bdabe9d2f9244005af0f566398e648da',
-    entryPointAddress: '0x5FF137D4b0FDCD49DcA30c7CF57E578a026d2789',
-    bundler: 'https://sepolia.voltaire.candidewallet.com/rpc',
+    name: network,
+    provider: provider || "https://mainnet.base.org/",
+    entryPointAddress: entryPointAddress,
+    bundler: bundler,
     baseAsset: {
       symbol: 'ETH',
       name: 'ETH',
