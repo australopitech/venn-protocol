272515d6c821efda591a0e20b3ed69fd4af5aebb
pbfranceschin <pbfranceschin@gmail.com>
2023-09-11 16:25:06 -0300

    dashboard-layout isConnected

--- dapp/components/dashboard/dashboard-layout/dashboard-layout.tsx
+++ dapp/components/dashboard/dashboard-layout/dashboard-layout.tsx
@@ -3,7 +3,7 @@ import classNames from 'classnames';
 import NavBar from '@/components/common/navbar/navbar'
 import SideBar from '@/components/dashboard/sidebar/sidebar'
 import NftArea from '@/components/dashboard/nft-area/nft-area'
-
+import { useSigner } from '@usedapp/core';
 
 export interface DashboardLayoutProps {
   address?: string;
@@ -11,12 +11,13 @@ export interface DashboardLayoutProps {
 
 export default function DashboardLayout ({ address }: DashboardLayoutProps) {
 
-  const isWalletConnected = true; //temp
-  
+  // const isWalletConnected = true; //temp
+  const signer = useSigner();
+
   return (
     <div className={styles.dashboard} >
       <NavBar navbarGridTemplate={styles.navbarGridTemplate} currentPage='dashboard' />
-      { (isWalletConnected && address)
+      { (signer && address)
         ? <div className={styles.contentGridTemplate}> 
             <SideBar address={address}/>
             <NftArea  /> 
