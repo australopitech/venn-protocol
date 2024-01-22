a754bb21b3b247ac39b78171842e5d4d9f443408
pbfranceschin <pbfranceschin@gmail.com>
2024-01-22 10:07:07 -0300

    contract renaming

--- blockchain/contracts/protocol/IMarketPlace.sol
+++ blockchain/contracts/protocol/IMarketPlace.sol
@@ -4,16 +4,16 @@ pragma solidity ^0.8.12;
 interface IMarketPlace {
     
     /**
-     *  @dev allows rWallet accounts to rent NFT listed in the Market Place contract
+     *  @dev allows smart accounts to rent NFT listed in the Market Place contract
      * 
      *  Words MUST, REQUIRE, SHOULD in accordance RFC-2119 (check https://www.rfc-editor.org/rfc/rfc2119.txt).
      *  
      *  Market Places MUST implement the following function and use it as the outer most method for renting NFT's
-     *  in order to be compliant with the rwallet protocol.
+     *  in order to be compliant with Venn protocol.
      * 
      *  Implementations MUST:
      * 
-     *  - REQUIRE caller IS rWallet account. This can be done via `isWallet` method in factory contract.
+     *  - REQUIRE caller IS a Venn Smart Account. This can be done via `isWallet` method in factory contract.
      *  - REQUIRE caller's OPERATOR COUNT for this contract be ZERO, i.e., caller account CANNOT have ANY operators set for this NFT contract:
      *  this can be done via `getOperatorCount` method in wallet contracts.
      *  
