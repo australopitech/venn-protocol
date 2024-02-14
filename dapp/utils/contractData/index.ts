c05124c4afd16ef7e4d79e90017dc005f6583b83
pbfranceschin <pbfranceschin@gmail.com>
2024-02-14 18:07:40 -0300

    baseGoerli -> polygonMumbai

--- dapp/utils/contractData/index.ts
+++ dapp/utils/contractData/index.ts
@@ -1,8 +1,12 @@
+import { polygonMumbai } from 'viem/chains';
 import contracts from './contracts.json';
 
 const base_goerli = 84531;
 
-export const factoryContract = contracts[base_goerli][0].contracts.SmartAccountFactory; // same address in: baseGoerli, PolygonMumbai
+export const factoryContract = contracts[polygonMumbai.id][0].contracts.SmartAccountFactory; // same address in: baseGoerli, PolygonMumbai
 //
-export const mktPlaceContract = contracts[base_goerli][0].contracts.MarketPlace;
-export const receiptsContract = contracts[base_goerli][0].contracts.ReceiptNFT;
+// export const mktPlaceContract = contracts[base_goerli][0].contracts.MarketPlace;
+// export const receiptsContract = contracts[base_goerli][0].contracts.ReceiptNFT;
+// 
+export const mktPlaceContract = contracts[polygonMumbai.id][0].contracts.MarketPlace;
+export const receiptsContract = contracts[polygonMumbai.id][0].contracts.ReceiptNFT;
