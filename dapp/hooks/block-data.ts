d355efe8f8a2158b9465974f6975857e4cec0469
pbfranceschin <pbfranceschin@gmail.com>
2024-02-16 16:48:08 -0300

    hooks: useLatestBlock useBaseFee

--- dapp/hooks/block-data.ts
+++ dapp/hooks/block-data.ts
@@ -1,9 +1,10 @@
 import { useEffect, useState } from "react";
 import { useBlockNumber, usePublicClient } from "wagmi";
+import { LatesBlockReturnType } from "@/types";
 
 export function useTimestamp () {
   const [timestamp, setTimestamp] = useState<bigint>();
-  const { data: block, isError, isLoading } = useBlockNumber();
+  const { data: block } = useBlockNumber();
   const client = usePublicClient();
   useEffect(() => {
     const resolveTimestamp = async () => {
@@ -12,4 +13,22 @@ export function useTimestamp () {
     }
   }, [block]);
   return timestamp;
+}
+
+export function useLatestBlock ({watch} : {watch?: boolean}) : LatesBlockReturnType {
+  const [data, setData] = useState<any>();
+  const { data: blockNumber, error, isLoading } = useBlockNumber({ watch });
+  const client = usePublicClient();
+  useEffect(() => {
+    const resolveBlock = async () => {
+      setData( await client.getBlock({ blockNumber }))
+    }
+    resolveBlock();
+  }, [blockNumber]);
+  return { data, error, isLoading }
+}
+
+export function useBaseFee ({watch} : {watch?: boolean}) {
+  const { data: block, error, isLoading } = useLatestBlock({ watch });
+  return { data: block?.baseFeePerGas, error, isLoading }
 }
