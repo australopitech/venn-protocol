8b28700db2e22ff6888e82e936d995c1d35f4bb7
pbfranceschin <pbfranceschin@gmail.com>
2024-02-22 15:54:44 -0300

    hooks: new arg: trigger

--- dapp/hooks/nft-data.tsx
+++ dapp/hooks/nft-data.tsx
@@ -1,12 +1,19 @@
 import { useState, useEffect, useMemo } from "react";
 import { useNetwork, usePublicClient } from "wagmi";
-import { getEndTime, getListData, getRealNft, ownerOf } from "@/utils/listing-data";
+import { getEndTime, getListData, getRealNft, ownerOf, isApproved } from "@/utils/listing-data";
 import { useTimestamp } from "./block-data";
+import { getMktPlaceContractAddress } from "@/utils/contractData";
 
 interface NftDataArgs {
-    contract?: `0x${string}`,
-    tokenId?: bigint,
+  contract?: `0x${string}`,
+  tokenId?: bigint,
+  owner?: `0x${string}`,
 }
+
+interface NftDataHookArgs extends NftDataArgs {
+  trigger?: boolean;
+}
+
 
 interface NftDataObj {
   contract: `0x${string}`,
@@ -82,6 +89,45 @@ export function useHolder(args? : NftDataArgs) {
     resolveHolder();
   }, [memoizedArgs]);
   
+  return { data, error, isLoading }
+}
+
+export function useIsAppoved(args?: NftDataHookArgs) {
+  const [data, setData] = useState<boolean>();
+  const [error, setError] = useState<any>(null);
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+  const client = usePublicClient();
+  const { chain } = useNetwork();
+  const memoizedArgs = useMemo(() => args, [
+    args?.contract, args?.tokenId, args?.owner, args?.trigger
+  ]);
+
+  useEffect(() => {
+    if(!args)
+      return
+    const { contract, tokenId, owner } = args;
+    if(!contract || !owner || tokenId === undefined)
+      return
+    const resolveIsApproved = async () => {
+      setError(null);
+      setIsLoading(true);
+      try {
+        setData(await isApproved(
+          client,
+          contract,
+          tokenId,
+          owner,
+          getMktPlaceContractAddress(chain?.id)
+        ))
+      } catch (err) {
+        console.error(err);
+        setError(err)
+      } finally {
+        setIsLoading(false)
+      }
+    }
+    resolveIsApproved()
+  }, [memoizedArgs]);
   return { data, error, isLoading }
 }
 
