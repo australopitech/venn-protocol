656e3542e4e211f0f15e0c7c5f10e090b8c6f7bc
pbfranceschin <pbfranceschin@gmail.com>
2024-02-23 18:38:17 -0300

    refactor: useTimeLeft, ui: loading state

--- dapp/components/common/dialog-not-owned-borrowed-description/dialog-not-owned-borrowed-description.tsx
+++ dapp/components/common/dialog-not-owned-borrowed-description/dialog-not-owned-borrowed-description.tsx
@@ -10,114 +10,103 @@ import { useAccount, useBlockNumber, usePublicClient } from 'wagmi';
 import { useTimestamp } from '@/hooks/block-data';
 import { useSmartAccount } from '@/app/account/venn-provider';
 import { timeLeftString } from '@/utils/utils';
+import { useTimeLeft } from '@/hooks/nft-data';
+import { LoadingComponent } from '../loading/loading';
 
 export interface DialogNotOwnedBorrowedDescriptionProps {
   address?: `0x${string}`;
-  nftItem?: NftItem;
+  contractAddress?: string;
+  tokenId?: bigint;
   isRental?: boolean;
 }
 
 const dayCutOff = 82800; // 23 h;
 const hourCutOff = 3540 // 59 min;
 
-async function _getEndTime(
-  client: any, 
-  account?: `0x${string}`, 
-  nftItem?: NftItem,
-) {
-  if(!account || !nftItem) return
-  if(!nftItem.nftData.token_id) 
-    throw new Error('no token id found');
-  let contractAddr: string;
-  let tokenId: bigint;
-  let acc: `0x${string}`;
-  if(getAddress(nftItem.contractAddress) === receiptsContract.address) {
-    const nftObj = await getNFTByReceipt(
-      client,
-      BigInt(nftItem.nftData.token_id)
-    );
-    contractAddr = nftObj.contractAddress;
-    tokenId = nftObj.tokenId;
-    acc = await ownerOf(client, contractAddr, tokenId);
-  } else {
-    contractAddr = nftItem.contractAddress;
-    tokenId = BigInt(nftItem.nftData.token_id);
-    acc = account;
-  }
-  console.log('getEndTime inputs', acc, contractAddr, tokenId)
-  return getEndTime(client, acc, contractAddr, tokenId)
-}
 
 export const DialogNotOwnedBorrowedDescription = ({ 
   address,
-  nftItem,
+  contractAddress,
+  tokenId,
   isRental
 }: DialogNotOwnedBorrowedDescriptionProps) => {
-  const [timeLeft, setTimeLeft] = useState<bigint>();
+  const [isLoadingData, setIsLoadingData] = useState(true);
+  // const [timeLeft, setTimeLeft] = useState<bigint>();
   // const [block, setBlock] = useState<GetBlockReturnType>();
-  const [isLoading, setIsLoading] = useState<boolean>();
-  const [error, setError] = useState<any>(null);
+  // const [isLoading, setIsLoading] = useState<boolean>();
+  // const [error, setError] = useState<any>(null);
   // const { address: eoa } = useAccount();
-  const { address: vsa } = useSmartAccount()
+  // const { address: vsa } = useSmartAccount()
   // const { data: blockNum, error: blockErr } = useBlockNumber({ watch: true });
-  const client = usePublicClient();
-  const timestampResponse = useTimestamp();
-  console.log('timestamp', timestampResponse.data)
-  
+  // const client = usePublicClient();
+  const timeLeft = useTimeLeft({
+    contract: contractAddress as `0x${string}`,
+    tokenId
+  });
 
   useEffect(() => {
-    const resolveTimeLeft = async() => {
-      const addr = address ?? vsa;
-      let endTime: bigint | undefined;
-      // setIsLoading(true)
-      try {
-        endTime = await _getEndTime(client, addr, nftItem);
-        console.log('endTime', endTime)  
-      } catch (err) {
-        console.error(err)
-        setError(err)
-      } 
-      // finally {
-      //   setIsLoading(false)
-      // }
-      // console.log('endTime', endTime?.toString());
-      if(timestampResponse.error)
-        setError(timestampResponse.error)
-      if(endTime && timestampResponse.data) 
-        setTimeLeft(endTime - timestampResponse.data)
-    }
+    if(!timeLeft.isLoading)
+      setTimeout(() => {
+        setIsLoadingData(false);
+      }, 2000);
+  }, [timeLeft])
+  
 
-    resolveTimeLeft();
-    console.log('timeLeft', timeLeft)
-  }, [vsa, client, timestampResponse]);
+  // useEffect(() => {
+  //   const resolveTimeLeft = async() => {
+  //     const addr = address ?? vsa;
+  //     let endTime: bigint | undefined;
+  //     // setIsLoading(true)
+  //     try {
+  //       endTime = await _getEndTime(client, addr, nftItem);
+  //       console.log('endTime', endTime)  
+  //     } catch (err) {
+  //       console.error(err)
+  //       setError(err)
+  //     } 
+  //     // finally {
+  //     //   setIsLoading(false)
+  //     // }
+  //     // console.log('endTime', endTime?.toString());
+  //     if(timestampResponse.error)
+  //       setError(timestampResponse.error)
+  //     if(endTime && timestampResponse.data) 
+  //       setTimeLeft(endTime - timestampResponse.data)
+  //   }
 
+  //   resolveTimeLeft();
+  //   console.log('timeLeft', timeLeft)
+  // }, [vsa, client, timestampResponse]);
+  console.log('timeLeft',timeLeft)
   return (
     <div className={styles.bodyDescriptionContainer}>
       <div className={styles.divider}></div>
       <div className={styles.bodyDescription}>
-        {
-        timeLeft
-          ? timeLeft > 0
+        {isLoadingData
+          ? <div style={{justifyContent: 'center'}}> <LoadingComponent /> </div>
+          : timeLeft.error
             ? <>
-            <span>
-            This NFT is {isRental? "rented by you" : "rented"}. The rent <span className={styles.textHilight}>expires in</span>
-            </span>
-            <span className={styles.timeLeftValue}> 
-              {timeLeftString(timeLeft)}
-            </span>
-            </>
-            : <>
-                <span>
-                This NFT is {isRental? 'rented by you' : 'rented'}.<br></br> The rent <span className={styles.textHilight}>is expired.</span>
-                </span>
-                </>
-          : error &&
-            <>
-            <span>
-              There was an error fectching info about the renting period. Please try again later <br/>
-              {error?.message}
-            </span>
-            </>
+              <span>
+                There was an error fectching info about the renting period. Please try again later <br/>
+                {timeLeft.error.message}
+              </span>
+              </>
+            : timeLeft.data
+              ? timeLeft.data > 0
+                ? <>
+                  <span>
+                  This NFT is {isRental? "rented by you" : "rented"}. The rent <span className={styles.textHilight}>expires in</span>
+                  </span>
+                  <span className={styles.timeLeftValue}> 
+                    {timeLeftString(timeLeft.data)}
+                  </span>
+                  </>
+                : <>
+                  <span>
+                  This NFT is {isRental? 'rented by you' : 'rented'}.<br></br> The rent <span className={styles.textHilight}>is expired.</span>
+                  </span>
+                  </>
+              : "Something went wrong"
         }
         {/** holder === signer.address &&
          *  <`Release NFT` button /> */}
