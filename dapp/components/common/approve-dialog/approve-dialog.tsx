50721958f2e804e140873352d292bbac9643ab6a
pbfranceschin <pbfranceschin@gmail.com>
2024-03-09 09:06:41 -0300

    ui: approve-dialog

--- dapp/components/common/approve-dialog/approve-dialog.tsx
+++ dapp/components/common/approve-dialog/approve-dialog.tsx
@@ -22,34 +22,54 @@ interface ButtonProps {
   onClick?: any
 }
 
-interface Event {
-  id: number
-  params: {
-    id: number
-    expiry: number
-    relays: Array<{
-      protocol: string
-      data?: string
-    }>
-    proposer: {
-      publicKey: string
-      metadata: {
-        name: string
-        description: string
-        url: string
-        icons: string[]
-      }
-    }
-    requiredNamespaces: Record<
-      string,
-      {
-        chains: string[]
-        methods: string[]
-        events: string[]
-      }
-    >
-    pairingTopic?: string
-  }
+// interface Event {
+//   id: number
+//   params: {
+//     id: number
+//     expiry: number
+//     relays: Array<{
+//       protocol: string
+//       data?: string
+//     }>
+//     proposer: {
+//       publicKey: string
+//       metadata: {
+//         name: string
+//         description: string
+//         url: string
+//         icons: string[]
+//       }
+//     }
+//     requiredNamespaces: Record<
+//       string,
+//       {
+//         chains: string[]
+//         methods: string[]
+//         events: string[]
+//       }
+//     >
+//     pairingTopic?: string
+//   }
+// }
+
+const ErrorIcon = () => {
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" className="icon icon-tabler icon-tabler-xbox-x" width="40" height="40" viewBox="0 0 24 24" strokeWidth="2" stroke="currentColor" fill="none" strokeLinecap="round" strokeLinejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M12 21a9 9 0 0 0 9 -9a9 9 0 0 0 -9 -9a9 9 0 0 0 -9 9a9 9 0 0 0 9 9z" /><path d="M9 8l6 8" /><path d="M15 8l-6 8" /></svg>
+  )
+}
+
+const SearchIcon = () => {
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" className="icon icon-tabler icon-tabler-search" width="24" height="24" viewBox="0 0 24 24" strokeWidth="2" stroke="currentColor" fill="none" strokeLinecap="round" strokeLinejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M10 10m-7 0a7 7 0 1 0 14 0a7 7 0 1 0 -14 0" /><path d="M21 21l-6 -6" /></svg>
+  )
+}
+
+const SuccessIcon = () => {
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" className="icon icon-tabler icon-tabler-circle-check" width="40" height="40" viewBox="0 0 24 24" strokeWidth="2" stroke="currentColor" fill="none" strokeLinecap="round" strokeLinejoin="round">
+      <path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M12 12m-9 0a9 9 0 1 0 18 0a9 9 0 1 0 -18 0" /><path d="M9 12l2 2l4 -4" />
+    </svg>
+  )
 }
 
 export const ApproveButton = ({onClick}: ButtonProps) => {
@@ -116,12 +136,12 @@ export default function ApproveDialog ({ onApprove, onReject, onClose, loading, 
                     </div>
                   : error
                     ? <div className={styles.approveDescriptionContainer}>
-                        <div className={styles.ErrorTitle}>An Error ocurred!</div>
-                        <div className={styles.ErrorDescription}> This error ocurred while processing your request: </div>
-                        <div className={styles.approveDescription}>{error.message}</div>
+                        <div className={styles.errorTitle}><ErrorIcon/> An Error ocurred!</div>
+                        <div className={styles.errorDescription}> This error ocurred while processing your request: </div>
+                        <div className={styles.errorMessage}>{error.message}</div>
                         {error.code === '001' && 
                           <a 
-                          className={styles.approveDescription}
+                          className={styles.txLink}
                           href={chain?.blockExplorers?.default.url + `/account/${vsa ?? eoa}`}
                           target='_blank'
                           > 
@@ -131,8 +151,15 @@ export default function ApproveDialog ({ onApprove, onReject, onClose, loading, 
                       </div>
                     : txResolved?.success
                       ? <div className={styles.approveDescriptionContainer}>
-                          <div className={styles.txTitle}>Transaction Successful!!</div>
-                          <div className={styles.txDescription}>tx hash: {txResolved.hash}</div>
+                          <div className={styles.txTitle}><SuccessIcon /> <span style={{paddingLeft: '4px'}}>Transaction Successful!!</span></div>
+                          <div className={styles.txDescription}>
+                            <div className={styles.txLinkContainer}>
+                              <SearchIcon />
+                              <a href={`https://mumbai.polygonscan.com/tx/${txResolved.hash}`} target='_blank' className={styles.txLink}>
+                              View it on block explorer.
+                              </a>
+                            </div>
+                          </div>
                           <div className={styles.buttonContainer}> <CloseButton onClick={() => onClose()}/> </div>
                         </div>
                       : <div className={styles.approveDescriptionContainer}>
