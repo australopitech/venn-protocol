f9763e302ac8b5213544e6c0929113f88158cd9d
pbfranceschin <pbfranceschin@gmail.com>
2024-03-29 09:44:59 -0300

    help-tooltip

--- dapp/components/common/tooltip/tooltip.tsx
+++ dapp/components/common/tooltip/tooltip.tsx
@@ -9,11 +9,24 @@ interface TooltipProps {
   children : React.ReactNode;
 }
 
-export default function Tooltip ({ text, children, style, position } : TooltipProps ) {
+export function Tooltip ({ text, children, style, position } : TooltipProps ) {
    return (
     <div className={classNames(position === 'top' ? styles.topTooltip : styles.tooltip, style)}>
         <span className={position === 'top' ? styles.topTooltiptext : styles.tooltiptext}>{text}</span>
         {children}
     </div>
    ) 
+}
+
+interface HelpTooltipProps {
+  text: string;
+  children: React.ReactNode;
+}
+export function HelpTooltip({text, children} : HelpTooltipProps) {
+  return (
+    <div className={styles.helpTooltip}>
+      <span className={styles.helpTooltiptext}>{text}</span>
+      {children}
+    </div>
+  )
 }
