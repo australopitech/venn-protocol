1f237341ec5b0b1cff3c7d41d85ee781ce8b74dc
pbfranceschin <pbfranceschin@gmail.com>
2024-04-16 15:17:38 -0300

    debug: sepolia logo

--- dapp/components/landing-page/chains/chains.tsx
+++ dapp/components/landing-page/chains/chains.tsx
@@ -1,3 +1,4 @@
+import classNames from 'classnames';
 import styles from './chains.module.css';
 import { Base, EthereumMain, EthereumSepolia, Polygon } from './graphics';
 import { Tooltip } from '@/components/common/tooltip/tooltip';
@@ -7,10 +8,12 @@ export default function AvailableChains () {
   return (
     <div className={styles.body}>
         <div className={styles.text}>Available with</div>
-        <Tooltip text='ETHEREUM SEPOLIA'><EthereumSepolia/></Tooltip>
-        <div className={styles.disabled}><EthereumMain /></div>
-        <div className={styles.disabled}><Base/></div>
-        <div className={styles.disabled}><Polygon/></div>
+        <div className={styles.chains}>
+          <div className={styles.logo}><Tooltip text='ETHEREUM SEPOLIA'><EthereumSepolia/></Tooltip></div>
+          <div className={classNames(styles.logo, styles.disabled)}><EthereumMain /></div>
+          <div className={classNames(styles.logo, styles.disabled)}><Base/></div>
+          <div className={classNames(styles.logo, styles.disabled)}><Polygon/></div>
+        </div>
     </div>
   )
 }
