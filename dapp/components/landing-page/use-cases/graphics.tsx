33b115cbec2e081023ca525127d6dc143ac72ec9
pbfranceschin <pbfranceschin@gmail.com>
2024-04-17 12:00:02 -0300

    use-cases: width responsive

--- dapp/components/landing-page/use-cases/graphics.tsx
+++ dapp/components/landing-page/use-cases/graphics.tsx
@@ -17,17 +17,93 @@ export const RightArrow = () => {
     )
 }
 
+/* 3 dots */
+
+// export const DotsFirst = () => {
+//     return (
+//         <svg viewBox="0 0 120 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+//             <g clip-path="url(#clip0_11_38)">
+//             <circle cx="9.90553" cy="9.90553" r="8.90553" fill="black" stroke="black" stroke-width="2"/>
+//             <circle cx="59.9567" cy="9.90553" r="8.90553" fill="#FCFCFC" stroke="black" stroke-width="2"/>
+//             <circle cx="110.008" cy="9.90553" r="8.90553" fill="#FCFCFC" stroke="black" stroke-width="2"/>
+//             </g>
+//             <defs>
+//             <clipPath id="clip0_11_38">
+//             <rect width="119.91" height="19.81" fill="white"/>
+//             </clipPath>
+//             </defs>
+//         </svg>
+//     )
+// }
+
+/* 4 dots */
+
 export const DotsFirst = () => {
     return (
-        <svg viewBox="0 0 120 20" fill="none" xmlns="http://www.w3.org/2000/svg">
-            <g clip-path="url(#clip0_11_38)">
+        <svg viewBox="0 0 170 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <g clip-path="url(#clip0_36_2682)">
             <circle cx="9.90553" cy="9.90553" r="8.90553" fill="black" stroke="black" stroke-width="2"/>
-            <circle cx="59.9567" cy="9.90553" r="8.90553" fill="#FCFCFC" stroke="black" stroke-width="2"/>
-            <circle cx="110.008" cy="9.90553" r="8.90553" fill="#FCFCFC" stroke="black" stroke-width="2"/>
+            <circle cx="59.9567" cy="9.90553" r="8.90553" stroke="black" stroke-width="2"/>
+            <circle cx="110.008" cy="9.90553" r="8.90553" stroke="black" stroke-width="2"/>
+            <circle cx="159.906" cy="9.90553" r="8.90553" stroke="black" stroke-width="2"/>
             </g>
             <defs>
-            <clipPath id="clip0_11_38">
-            <rect width="119.91" height="19.81" fill="white"/>
+            <clipPath id="clip0_36_2682">
+            <rect width="169.81" height="19.81" fill="white"/>
+            </clipPath>
+            </defs>
+        </svg>
+    )
+}
+
+export const DotsSecond = () => {
+    return (
+        <svg viewBox="0 0 170 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <g clip-path="url(#clip0_36_2683)">
+            <circle cx="9.90553" cy="9.90553" r="8.90553" stroke="black" stroke-width="2"/>
+            <circle cx="59.9567" cy="9.90553" r="8.90553" fill="black" stroke="black" stroke-width="2"/>
+            <circle cx="110.008" cy="9.90553" r="8.90553" stroke="black" stroke-width="2"/>
+            <circle cx="159.906" cy="9.90553" r="8.90553" stroke="black" stroke-width="2"/>
+            </g>
+            <defs>
+            <clipPath id="clip0_36_2683">
+            <rect width="169.81" height="19.81" fill="white"/>
+            </clipPath>
+            </defs>
+        </svg>
+    )
+}
+
+export const DotsThird = () => {
+    return (
+        <svg viewBox="0 0 170 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <g clip-path="url(#clip0_36_2688)">
+            <circle cx="9.90553" cy="9.90553" r="8.90553" stroke="black" stroke-width="2"/>
+            <circle cx="59.9567" cy="9.90553" r="8.90553" stroke="black" stroke-width="2"/>
+            <circle cx="110.008" cy="9.90553" r="8.90553" fill="black" stroke="black" stroke-width="2"/>
+            <circle cx="159.906" cy="9.90553" r="8.90553" stroke="black" stroke-width="2"/>
+            </g>
+            <defs>
+            <clipPath id="clip0_36_2688">
+            <rect width="169.81" height="19.81" fill="white"/>
+            </clipPath>
+            </defs>
+        </svg>
+    )
+}
+
+export const DotsFourth = () => {
+    return (
+        <svg viewBox="0 0 170 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <g clip-path="url(#clip0_36_2693)">
+            <circle cx="9.90553" cy="9.90553" r="8.90553" stroke="black" stroke-width="2"/>
+            <circle cx="59.9567" cy="9.90553" r="8.90553" stroke="black" stroke-width="2"/>
+            <circle cx="110.008" cy="9.90553" r="8.90553" stroke="black" stroke-width="2"/>
+            <circle cx="159.906" cy="9.90553" r="8.90553" fill="black" stroke="black" stroke-width="2"/>
+            </g>
+            <defs>
+            <clipPath id="clip0_36_2693">
+            <rect width="169.81" height="19.81" fill="white"/>
             </clipPath>
             </defs>
         </svg>
--- dapp/components/landing-page/use-cases/use-cases.tsx
+++ dapp/components/landing-page/use-cases/use-cases.tsx
@@ -1,24 +1,39 @@
 'use client'
 import styles from "./use-cases.module.css";
-import { RightArrow, LeftArrow, DotsFirst } from "./graphics";
+import { RightArrow, LeftArrow, DotsFirst, DotsSecond, DotsThird, DotsFourth } from "./graphics";
 import { orbitron } from "@/app/fonts";
 import classNames from "classnames";
+import { useState } from "react";
 
 
-function Dots ({ placement } : { placement : 1 | 2 | 3 }) {
+function Dots ({ placement } : { placement : 1 | 2 | 3 | 4}) {
     if(placement === 1)
-        return <DotsFirst/>
+      return <DotsFirst/>
+    else if(placement === 2)
+      return <DotsSecond/>
+    else if(placement === 3)
+      return <DotsThird/>
+    else if(placement === 4)
+      return <DotsFourth/>
 }
 
 
-function Card ({ cardNum } : { cardNum : 1 | 2 | 3 }) {
+function Card ({ cardNum } : { cardNum : 1 | 2 | 3 | 4}) {
 
   return (
     <div className={styles.gameCard}>
         <div className={styles.text}>
-          <div className={classNames(styles.cardTitle, orbitron.className)}>Gaming</div>
+          <div className={classNames(styles.cardTitle, orbitron.className)}>
+            <div className={styles.arrowNarrowContainer}>
+                <div className={styles.arrowNarrow}><LeftArrow/></div>
+            </div>
+            Gaming
+            <div className={styles.arrowNarrowContainer}>
+              <div className={styles.arrowNarrow}><RightArrow/></div>
+            </div>
+          </div>
           <div className={styles.cardDescription}>
-              Perfect to use with in-game items. Rent out CHARACTERS, WEAPONS, LAND, VEHICLES, or ANY item manifested as an NFT. TRY IT OUT before you buy it. Or make your asset PROFIT for you!
+              Perfect to use with in-game items. Rent out <b>CHARACTERS, WEAPONS, LAND, VEHICLES,</b> or <b>ANY</b> item manifested as an NFT. <b>TRY IT OUT</b> before you buy it. Or make your asset <b>PROFIT</b> for you!
           </div>
         </div>
         <div className={styles.dotsContainer}>
@@ -30,6 +45,7 @@ function Card ({ cardNum } : { cardNum : 1 | 2 | 3 }) {
 
 
 export default function UseCases () {
+  const [position, setPosition] = useState<1 | 2 | 3 | 4>(1);
 
   return (
     <div className={styles.body}>
@@ -37,7 +53,7 @@ export default function UseCases () {
             <div className={styles.arrowContainer}>
                 <div className={classNames(styles.arrow, styles.disabled)}><LeftArrow/></div>
             </div>
-            <Card cardNum={1}/>
+            <Card cardNum={position}/>
             <div className={styles.arrowContainer}>
                 <div className={styles.arrow}><RightArrow/></div>
             </div>
