ccae5a682efa6de0605eb002d40bf61aab0c09db
pbfranceschin <pbfranceschin@gmail.com>
2024-04-17 13:34:31 -0300

    use cases: 4 cards

--- dapp/components/landing-page/use-cases/use-cases.tsx
+++ dapp/components/landing-page/use-cases/use-cases.tsx
@@ -18,18 +18,91 @@ function Dots ({ placement } : { placement : 1 | 2 | 3 | 4}) {
 }
 
 
-function Card ({ cardNum } : { cardNum : 1 | 2 | 3 | 4}) {
-
+function AccessCard ({ setPosition } : { setPosition: any }) {
   return (
-    <div className={styles.gameCard}>
+    <div className={classNames(styles.card, styles.accessCard)}>
         <div className={styles.text}>
           <div className={classNames(styles.cardTitle, orbitron.className)}>
             <div className={styles.arrowNarrowContainer}>
-                <div className={styles.arrowNarrow}><LeftArrow/></div>
+                <div className={classNames(styles.arrowNarrow)} onClick={() => setPosition(3)}><LeftArrow/></div>
+            </div>
+            Access Tokens
+            <div className={styles.arrowNarrowContainer}>
+              <div className={classNames(styles.arrowNarrow, styles.disabled)}><RightArrow/></div>
+            </div>
+          </div>
+          <div className={styles.cardDescription}>
+          <b>TICKETS, CREDENTIALS, CLUB MEMBERSHIPS</b> can now also change hands without worry. Take a <b>SNEAK PEAK</b> at that exclusive club or <b> GET PAID</b> for not attending your favorite event!
+          </div>
+        </div>
+        <div className={styles.dotsContainer}>
+            <Dots placement={4}/>
+        </div>
+    </div>
+  )
+}
+
+function DomainCard ({ setPosition } : { setPosition: any }) {
+  return (
+    <div className={classNames(styles.card, styles.domainCard)}>
+        <div className={styles.text}>
+          <div className={classNames(styles.cardTitle, orbitron.className)}>
+            <div className={styles.arrowNarrowContainer}>
+                <div className={classNames(styles.arrowNarrow)} onClick={() => setPosition(2)}><LeftArrow/></div>
+            </div>
+            Domains
+            <div className={styles.arrowNarrowContainer}>
+              <div className={styles.arrowNarrow} onClick={() => setPosition(4)}><RightArrow/></div>
+            </div>
+          </div>
+          <div className={styles.cardDescription}>
+          A <b>NEW LAYER</b> in the market for domain names. These assets will suddenly become <b>ATTRACTIVE</b> to <b>INVESTORS</b>. Pick a valuable name and <b>GET PAID</b>!
+          </div>
+        </div>
+        <div className={styles.dotsContainer}>
+            <Dots placement={3}/>
+        </div>
+    </div>
+  )
+}
+
+function MetaverseCard ({ setPosition } : { setPosition: any }) {
+  return (
+    <div className={classNames(styles.card, styles.metaverseCard)}>
+        <div className={styles.text}>
+          <div className={classNames(styles.cardTitle, orbitron.className)}>
+            <div className={styles.arrowNarrowContainer}>
+                <div className={classNames(styles.arrowNarrow)} onClick={() => setPosition(1)}><LeftArrow/></div>
+            </div>
+            Metaverse
+            <div className={styles.arrowNarrowContainer}>
+              <div className={styles.arrowNarrow} onClick={() => setPosition(3)}><RightArrow/></div>
+            </div>
+          </div>
+          <div className={styles.cardDescription}>
+          <b>ANY</b> asset within the Metaverse will immediately become <b>CAPITAL</b>. From <b>REAL-ESTATE</b>, to <b>CREDENTIALS</b>, to <b>SKINS</b> and even expensive clothing. Prepare for a booming economy!
+          </div>
+        </div>
+        <div className={styles.dotsContainer}>
+            <Dots placement={2}/>
+        </div>
+    </div>
+  )
+}
+
+
+function GamerCard ({ setPosition } : { setPosition: any }) {
+
+  return (
+    <div className={classNames(styles.card, styles.gameCard)}>
+        <div className={styles.text}>
+          <div className={classNames(styles.cardTitle, orbitron.className)}>
+            <div className={styles.arrowNarrowContainer}>
+                <div className={classNames(styles.arrowNarrow, styles.disabled)}><LeftArrow/></div>
             </div>
             Gaming
             <div className={styles.arrowNarrowContainer}>
-              <div className={styles.arrowNarrow}><RightArrow/></div>
+              <div className={styles.arrowNarrow} onClick={() => setPosition(2)}><RightArrow/></div>
             </div>
           </div>
           <div className={styles.cardDescription}>
@@ -37,25 +110,49 @@ function Card ({ cardNum } : { cardNum : 1 | 2 | 3 | 4}) {
           </div>
         </div>
         <div className={styles.dotsContainer}>
-            <Dots placement={cardNum}/>
+            <Dots placement={1}/>
         </div>
     </div>
   )
 }
 
+function Card ({ cardNum, setPosition } : { cardNum: number, setPosition: any}) {
+  return(
+    <>
+    {cardNum === 1
+     ? <GamerCard setPosition={setPosition}/>
+     : cardNum === 2
+      ? <MetaverseCard setPosition={setPosition}/>
+      : cardNum === 3
+       ? <DomainCard setPosition={setPosition}/>
+       : cardNum === 4
+        ? <AccessCard setPosition={setPosition}/>
+        : "Error"
+    }
+    </>
+  )
+}
+
 
 export default function UseCases () {
-  const [position, setPosition] = useState<1 | 2 | 3 | 4>(1);
+  const [position, setPosition] = useState<number>(1);
+  console.log('position', position);
 
   return (
     <div className={styles.body}>
         <div className={styles.content}>
             <div className={styles.arrowContainer}>
-                <div className={classNames(styles.arrow, styles.disabled)}><LeftArrow/></div>
+                <div 
+                className={classNames(styles.arrow, position === 1 ? styles.disabled : '')}
+                onClick={position === 1 ? () => {} : () => setPosition(position - 1)}
+                ><LeftArrow/></div>
             </div>
-            <Card cardNum={position}/>
+            <Card cardNum={position} setPosition={setPosition}/>
             <div className={styles.arrowContainer}>
-                <div className={styles.arrow}><RightArrow/></div>
+                <div
+                className={classNames(styles.arrow, position === 4 ? styles.disabled : '')}
+                onClick={position === 4 ? () => {} : () => setPosition(position + 1)}
+                ><RightArrow/></div>
             </div>
         </div>
     </div>
