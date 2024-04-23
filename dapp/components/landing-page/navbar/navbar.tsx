2c3c2805cefbfaff5e5f03bff65764bebb188bbf
pbfranceschin <pbfranceschin@gmail.com>
2024-04-23 10:04:24 -0300

    new: dropdown menu

--- dapp/components/landing-page/navbar/navbar.tsx
+++ dapp/components/landing-page/navbar/navbar.tsx
@@ -1,17 +1,42 @@
+'use client'
 import classNames from "classnames";
 import { NewLogoPlain } from "../../common/logo/logo";
 import styles from "./navbar.module.css";
 import Link from "next/link";
 import { source_code_pro } from "@/app/fonts";
+import { useState, useRef, useEffect, useCallback } from "react";
+import { useRouter } from "next/navigation";
+import { motion, AnimatePresence, Variants } from "framer-motion";
 
 const MenuIcon = () => {
     return (
-      <svg width="36px" height="36px" viewBox="0 0 1.08 1.08" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M0.18 0.315a0.045 0.045 0 0 1 0.045 -0.045h0.63a0.045 0.045 0 1 1 0 0.09H0.225a0.045 0.045 0 0 1 -0.045 -0.045zm0 0.225a0.045 0.045 0 0 1 0.045 -0.045h0.63a0.045 0.045 0 1 1 0 0.09H0.225a0.045 0.045 0 0 1 -0.045 -0.045zm0 0.225a0.045 0.045 0 0 1 0.045 -0.045h0.63a0.045 0.045 0 1 1 0 0.09H0.225a0.045 0.045 0 0 1 -0.045 -0.045z" fill="#5E5E5E"/></svg>
+      <svg width="100%" height="100%" viewBox="0 0 1.08 1.08" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M0.18 0.315a0.045 0.045 0 0 1 0.045 -0.045h0.63a0.045 0.045 0 1 1 0 0.09H0.225a0.045 0.045 0 0 1 -0.045 -0.045zm0 0.225a0.045 0.045 0 0 1 0.045 -0.045h0.63a0.045 0.045 0 1 1 0 0.09H0.225a0.045 0.045 0 0 1 -0.045 -0.045zm0 0.225a0.045 0.045 0 0 1 0.045 -0.045h0.63a0.045 0.045 0 1 1 0 0.09H0.225a0.045 0.045 0 0 1 -0.045 -0.045z" fill="#000000"/></svg>
     )
 }
 
+const CloseIcon = () => {
+    return (
+        <svg  xmlns="http://www.w3.org/2000/svg"  width="100%"  height="100%"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  strokeWidth="2"  strokeLinecap="round"  strokeLinejoin="round"  className="icon icon-tabler icons-tabler-outline icon-tabler-x"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M18 6l-12 12" /><path d="M6 6l12 12" /></svg>
+    )
+}
+
+const dropdownItems = ['ABOUT', 'GUIDES', 'CONTACT US', 'LAUNCH MARKETPLACE', 'LAUNCH DASHBOARD'];
 
 export default function NavBar () {
+  const router = useRouter();
+
+  const handleItemSelect = useCallback((item: string) => {
+    if(item === 'ABOUT')
+        window.open('https://australopitech.gitbook.io/venn', '_blank');
+    if(item === 'GUIDES')
+        window.open('https://australopitech.gitbook.io/venn/overview/guides', '_blank');
+    if(item === 'CONTACT US')
+        router.push('/contact');
+    if(item === 'LAUNCH MARKETPLACE')
+        router.push('/');
+    if(item === 'LAUNCH DASHBOARD')
+        router.push('/dashboard');        
+  }, []) 
 
   return (
     <div className={styles.navbar}>
@@ -36,8 +61,103 @@ export default function NavBar () {
             </Link>
         </div>
         <div className={styles.menuIcon}>
-                <MenuIcon/>
+                <DropdownMenu  items={dropdownItems} onItemSelect={handleItemSelect}/>
         </div>
     </div>
   )
 }
+
+interface DropdownProps {
+    items: string[];
+    onItemSelect: (item: string) => void;
+}
+
+const itemVariants: Variants = {
+  open: {
+    opacity: 1,
+    y: 0,
+    transition: { type: "spring", stiffness: 300, damping: 24 }
+  },
+  closed: { opacity: 0, y: -10, transition: { duration: 0.1 } }
+};
+
+
+const DropdownMenu = ({ items, onItemSelect } : DropdownProps ) => {
+    const [isOpen, setIsOpen] = useState(false);
+    const dropdownRef = useRef<HTMLDivElement | null>(null);
+  
+    useEffect(() => {
+      // Function to check if clicked outside of the dropdown menu
+      const handleClickOutside = (event: MouseEvent) => {
+        if (dropdownRef.current && !dropdownRef.current.contains(event.target as Node)) {
+          setIsOpen(false);
+        }
+      };
+  
+      // Attach the listeners on component mount
+      document.addEventListener('mousedown', handleClickOutside);
+  
+      // Detach the listeners on component unmount
+      return () => {
+        document.removeEventListener('mousedown', handleClickOutside);
+      };
+    }, []);
+  
+    return (
+        <div className={styles.dropdownMenu} ref={dropdownRef}>
+          <div onClick={() => setIsOpen(!isOpen)}>
+            <AnimatePresence mode="wait" initial={false}>
+              {isOpen ? (
+                <motion.div className={styles.icon}
+                  key="close"
+                  initial={{ opacity: 0, scale: 0 }}
+                  animate={{ opacity: 1, scale: 1 }}
+                  exit={{ opacity: 0, scale: 0 }}
+                  transition={{ duration: 0.2 }}
+                >
+                  <CloseIcon />
+                </motion.div>
+              ) : (
+                <motion.div className={styles.icon}
+                  key="menu"
+                  initial={{ opacity: 0, scale: 0 }}
+                  animate={{ opacity: 1, scale: 1 }}
+                  exit={{ opacity: 0, scale: 0 }}
+                  transition={{ duration: 0.2 }}
+                >
+                  <MenuIcon />
+                </motion.div>
+              )}
+            </AnimatePresence>
+        </div>
+        <AnimatePresence>
+        {isOpen && (
+          <motion.ul
+            className={styles.dropdown}
+            initial="closed"
+            animate="open"
+            exit="closed"
+            variants={{
+              open: {
+                scale: 1,
+                transition: { staggerChildren: 0.05, delayChildren: 0.2 }
+              },
+              closed: { scale: 0 }
+            }}
+            style={{ originX: 1, originY: 0 }} // Top right corner
+          >
+            {items.map((item, index) => (
+              <motion.li
+                key={index}
+                variants={itemVariants}
+                onClick={() => onItemSelect(item)}
+              >
+                {item}
+              </motion.li>
+            ))}
+          </motion.ul>
+        )}
+        </AnimatePresence>
+    </div>
+    );
+  }
