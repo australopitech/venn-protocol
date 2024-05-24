40e4946a0a18ba229bd55b0686670597d1ad1a1a
pbfranceschin <pbfranceschin@gmail.com>
2024-05-24 16:24:54 -0300

    slider-dots

---
+++ dapp/components/market/slider-dots/slider-dots.tsx
@@ -0,0 +1,54 @@
+'use client'
+import styles from "./slider-dots.module.css";
+import { useEffect } from "react";
+import {  useAnimate } from "framer-motion";
+
+
+export const Dot = () => {
+    return (
+        <svg width="100%" height="100%" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <circle cx="9.90553" cy="9.90553" r="8.90553" fill="black" stroke="black" strokeWidth="2"/>
+        </svg>
+    )
+}
+
+export function Dots ({ placement } : { placement: number }) {
+  const [scope0, animate0] = useAnimate()
+  const [scope1, animate1] = useAnimate()
+  const [scope2, animate2] = useAnimate()
+  const [scope3, animate3] = useAnimate()
+
+  useEffect(() => {
+    if(placement === 0) {
+      animate0(scope0.current, { opacity: 1, scale: 1.5 });
+      animate1(scope1.current, { opacity: 0.2, scale: 1 });
+      animate3(scope3.current, { opacity: 0.2, scale: 1 });
+    }
+    if(placement === 1){
+      animate1(scope1.current, { opacity: 1, scale: 1.5 });
+      animate0(scope0.current, { opacity: 0.2, scale: 1 });
+      animate2(scope2.current, { opacity: 0.2, scale: 1 });
+    }
+    if(placement === 2){
+      animate2(scope2.current, { opacity: 1, scale: 1.5 });
+      animate3(scope3.current, { opacity: 0.2, scale: 1 });
+      animate1(scope1.current, { opacity: 0.2, scale: 1 });
+      
+    }
+    if(placement === 3){
+      animate3(scope3.current, { opacity: 1, scale: 1.5 });
+      animate0(scope0.current, { opacity: 0.2, scale: 1 });
+      animate2(scope2.current, { opacity: 0.2, scale: 1 });
+    }
+  }, [placement])
+
+  
+  return (
+    <div className={styles.dots}>
+      <div ref={scope0} style={{ opacity: 0.2 }}><Dot/></div>
+      <div ref={scope1} style={{ opacity: 0.2 }}><Dot/></div>
+      <div ref={scope2} style={{ opacity: 0.2 }}><Dot/></div>
+      {/* <div ref={scope3} style={{ opacity: 0.2 }}><Dot/></div> */}
+    </div>
+    )
+}
