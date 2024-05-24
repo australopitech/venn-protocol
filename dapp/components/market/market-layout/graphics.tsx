20faba54a27a26ca56bcd9544b5ad6cc6fe47411
pbfranceschin <pbfranceschin@gmail.com>
2024-05-24 16:26:22 -0300

    arrows

---
+++ dapp/components/market/market-layout/graphics.tsx
@@ -0,0 +1,20 @@
+
+export const CirleArrowBiColorLeft = () => {
+    return (
+        <svg width="100%" height="100%" viewBox="0 0 85 89" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M41 85.4201C63.5514 85.4201 81.8333 67.1382 81.8333 44.5868C81.8333 22.0351 63.5514 3.75345 41 3.75345C18.4484 3.75345 0.166687 22.0351 0.166687 44.5868C0.166687 67.1382 18.4484 85.4201 41 85.4201Z" fill="#D9D9D9"/>
+            <path d="M55.2916 44.5868H30.7916" stroke="#292D32" stroke-Width="6.125" strokeLinecap="round" strokeLinejoin="round"/>
+            <path d="M38.9583 32.3368L26.7083 44.5868L38.9583 56.8368" stroke="#292D32" stroke-Width="6.125" strokeLinecap="round" strokeLinejoin="round"/>
+        </svg>
+    )
+}
+
+export const CirleArrowBiColorRight = () => {
+    return (
+        <svg width="100%" height="100%" viewBox="0 0 85 89" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M43.9999 85.4201C21.4485 85.4201 3.1666 67.1382 3.1666 44.5868C3.1666 22.0351 21.4485 3.75345 43.9999 3.75345C66.5516 3.75345 84.8333 22.0351 84.8333 44.5868C84.8333 67.1382 66.5516 85.4201 43.9999 85.4201Z" fill="#D9D9D9"/>
+            <path d="M29.7085 44.5868H54.2085" stroke="#292D32" strokeWidth="6.125" strokeLinecap="round" strokeLinejoin="round"/>
+            <path d="M46.0417 32.3368L58.2917 44.5868L46.0417 56.8368" stroke="#292D32" strokeWidth="6.125" strokeLinecap="round" strokeLinejoin="round"/>
+        </svg>
+    )
+}
