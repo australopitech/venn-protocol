a17707b7fd4c79cc2b1d8c4d3d57a4fdeee3dc02
pbfranceschin <pbfranceschin@gmail.com>
2024-05-25 08:24:22 -0300

    new-ui: common navbar

--- dapp/components/common/navbar/navbar.tsx
+++ dapp/components/common/navbar/navbar.tsx
@@ -8,7 +8,6 @@ import Link from 'next/link';
 
 import { useRouter } from 'next/navigation';
 import { Tooltip } from '../tooltip/tooltip';
-import { source_code_pro } from '@/app/fonts';
 import ConnectButton from './connect-button';
 import { MenuIcon, MarketIcon, SearchIcon, DashboardIcon } from './icons';
 
@@ -97,52 +96,54 @@ export default function NavBar ({ navbarGridTemplate, currentPage }: NavBarProps
 
   return (
     // <div className={classNames(styles.navbar, styles.navbarGridTemplate, scrolled ? styles.navbarScrolled : '')}>
-    <div className={classNames(styles.navbar, styles.navbarGridTemplate, source_code_pro.className)}>
-      <div className={styles.itemsGroupLeft}>
-        <Link href="/" className={styles.logoLarge}>
-            <div style={{ width: "42px"}}>
-              <NewLogoPlain/>
+    <div className={styles.navbarOutter}>
+      <div className={styles.navbar}>
+        <div className={styles.itemsGroupLeft}>
+          <Link href="/" className={styles.logoLarge}>
+              <div style={{ width: "42px"}}>
+                <NewLogoPlain/>
+              </div>
+              <div style={{ width: "104px"}}>
+                <Name/>
+              </div>
+          </Link>
+          <Link href="/" className={styles.logoSmall}>
+                <NewLogoPlain/>            
+          </Link>
+          <div className={styles.searchBoxContainer}>
+            <Tooltip style={styles.fontFamily} text='Search comming soon'><SearchBox /></Tooltip>
+          </div>
+          <div className={styles.searchIcon} style={{ opacity: "0.3"}}>
+            <Tooltip style={styles.fontFamily} text='Search comming soon'><SearchIcon /></Tooltip>
+          </div>
+        </div>
+        <div className={styles.itemsGroupRight}>
+          <div className={styles.pageIcon}>
+            {currentPage === 'dashboard'
+              ? <MarketIcon/>      
+              : <DashboardIcon/>
+            }
+          </div>
+          <div className={styles.navItems}>
+            <div 
+                className={classNames(styles.secondaryButton, currentPage === 'market'? styles.active : '')}
+              >
+                <Link href="/">
+                  MARKET
+                </Link>
             </div>
-            <div style={{ width: "104px"}}>
-              <Name/>
-            </div>
-        </Link>
-        <Link href="/" className={styles.logoSmall}>
-              <NewLogoPlain/>            
-        </Link>
-        <div className={styles.searchBoxContainer}>
-          <Tooltip style={styles.fontFamily} text='Search comming soon'><SearchBox /></Tooltip>
-        </div>
-        <div className={styles.searchIcon} style={{ opacity: "0.3"}}>
-          <Tooltip style={styles.fontFamily} text='Search comming soon'><SearchIcon /></Tooltip>
-        </div>
-      </div>
-      <div className={styles.itemsGroupRight}>
-        <div className={styles.pageIcon}>
-          {currentPage === 'dashboard'
-            ? <MarketIcon/>      
-            : <DashboardIcon/>
-          }
-        </div>
-        <div className={styles.navItems}>
-          <div 
-              className={classNames(styles.secondaryButton, currentPage === 'market'? styles.active : '')}
+            <div 
+              className={classNames(styles.secondaryButton, currentPage === 'dashboard'? styles.active : '')}
             >
-              <Link href="/">
-                MARKET
+              <Link href="/dashboard"> 
+                DASHBOARD
               </Link>
+            </div>
           </div>
-          <div 
-            className={classNames(styles.secondaryButton, currentPage === 'dashboard'? styles.active : '')}
-          >
-            <Link href="/dashboard"> 
-              DASHBOARD
-            </Link>
+          <ConnectButton />
+          <div className={styles.iconButton}>
+            <DropdownMenu items={items} onItemSelect={handleItemSelect} />
           </div>
-        </div>
-        <ConnectButton />
-        <div className={styles.iconButton}>
-           <DropdownMenu items={items} onItemSelect={handleItemSelect} />
         </div>
       </div>
     </div>
