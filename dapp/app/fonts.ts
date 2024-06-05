81af7e40141a249598448629acd2194187f19a8e
pbfranceschin <pbfranceschin@gmail.com>
2024-06-05 15:50:33 -0300

    star-wars card

--- dapp/app/fonts.ts
+++ dapp/app/fonts.ts
@@ -1,4 +1,4 @@
-import { Passion_One, Tilt_Neon, Source_Code_Pro , Audiowide, Zilla_Slab_Highlight, Source_Sans_3, Staatliches } from 'next/font/google';
+import { Passion_One, Tilt_Neon, Source_Code_Pro , Audiowide, Zilla_Slab_Highlight, Source_Sans_3, Staatliches, Press_Start_2P } from 'next/font/google';
 
 export const passion_one = Passion_One({ weight: "700" ,subsets: ['latin'] });
 export const tilt_neon = Tilt_Neon({ subsets: ['latin'] });
@@ -8,3 +8,4 @@ export const audiowide = Audiowide({ weight: "400", subsets: ['latin']});
 export const zilla = Zilla_Slab_Highlight({weight: '700', subsets: ['latin']});
 export const source_sans_3 = Source_Sans_3({ subsets: ['latin'] });
 export const staatliches = Staatliches({ weight: "400", subsets: ['latin'] });
+export const press_start_2p = Press_Start_2P({ weight: '400', subsets: ['latin'] })
--- dapp/components/market/highlight-slide/cards.tsx
+++ dapp/components/market/highlight-slide/cards.tsx
@@ -1,8 +1,8 @@
 import classNames from 'classnames';
 import { RightArrow } from '../banner/graphics';
 import styles from './cards.module.css';
-import { BookInfo, IcyMountainsLocation } from './graphics';
-import { source_sans_3, staatliches } from '@/app/fonts';
+import { BookInfo, IcyMountainsLocation, StarWarsLogo } from './graphics';
+import { press_start_2p, source_sans_3, staatliches } from '@/app/fonts';
 
 
 
@@ -59,6 +59,43 @@ export function CardLearnMore () {
                 </div>
                 <div className={styles.book}>
                     <BookInfo/>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export function CardStarWars () {
+    return (
+        <div className={styles.card}>
+            <div className={styles.starWarsCard}>
+                <div className={styles.backgroundImg}></div>
+                <div className={styles.collectionImg}>
+                    <img 
+                    src='https://ipfs.filebase.io/ipfs/QmUGudTwYXZzSFTHgycQbdGa6fRbAoxRTbR9ptExVKNweH' 
+                    alt='collection image' 
+                    width='100%'
+                    height='100%'
+                    />
+                </div>
+                <div className={styles.starWarsDescription}>
+                    <div className={styles.starWarsName}>
+                        <div className={styles.starWarsLogo}><StarWarsLogo/></div>
+                        <div className={press_start_2p.className} style={{ color: '#ffffff', fontSize: 'var(--step-3)', WebkitTextStroke: '1px #000000'}}>
+                            Mock NFTs    
+                        </div>    
+                    </div>
+                    <div className={press_start_2p.className} style={{ 
+                        fontSize: 'var(--step--2)', 
+                        textWrap: 'balance', 
+                        WebkitTextStroke: '1px #000000', 
+                        color: '#FFC83C', 
+                        textAlign: 'center',
+                        maxWidth: 'calc(8.8*var(--step-3))'    
+                    }}>
+                        Demonstrative purpose only. We are not selling any!!
+                    </div>
+                    <div style={{ color: '#d9d9d9'}}><CheckoutListings/></div>
                 </div>
             </div>
         </div>
