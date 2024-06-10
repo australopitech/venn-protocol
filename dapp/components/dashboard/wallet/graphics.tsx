62e6d0e0c53fcfd4f61a45c7ca402d8544b3138c
pbfranceschin <pbfranceschin@gmail.com>
2024-06-10 10:13:19 -0300

    wallet-actions

---
+++ dapp/components/dashboard/wallet/graphics.tsx
@@ -0,0 +1,73 @@
+
+export const SendIcon = () => {
+    return (
+        <svg width="100%" height="100%" viewBox="0 0 24 25" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <g clip-path="url(#clip0_41_4202)">
+            <path d="M10 14.982L21 3.98196" stroke="black" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+            <path d="M21.0001 3.98196L14.5001 21.982C14.4562 22.0777 14.3858 22.1588 14.2971 22.2157C14.2085 22.2726 14.1054 22.3028 14.0001 22.3028C13.8948 22.3028 13.7917 22.2726 13.703 22.2157C13.6144 22.1588 13.5439 22.0777 13.5001 21.982L10.0001 14.982L3.00007 11.482C2.90433 11.4381 2.8232 11.3676 2.76632 11.279C2.70944 11.1904 2.6792 11.0873 2.6792 10.982C2.6792 10.8766 2.70944 10.7735 2.76632 10.6849C2.8232 10.5963 2.90433 10.5258 3.00007 10.482L21.0001 3.98196Z" stroke="black" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+            </g>
+            <defs>
+            <clipPath id="clip0_41_4202">
+            <rect width="24" height="24" fill="white" transform="translate(0 0.981964)"/>
+            </clipPath>
+            </defs>
+    </svg>
+    )
+}
+
+export const ConnectIcon = () => {
+
+    return (
+        <svg width="100%" height="100%" viewBox="0 0 25 25" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <g clip-path="url(#clip0_41_4193)">
+            <path d="M9.5 15.982L15.5 9.98196" stroke="black" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+            <path d="M11.5 6.98196L11.963 6.44596C12.4273 5.98153 12.9786 5.61311 13.5853 5.36175C14.192 5.1104 14.8423 4.98103 15.499 4.98103C16.1557 4.98103 16.806 5.1104 17.4127 5.36175C18.0194 5.61311 18.5707 5.98153 19.035 6.44596C19.5002 6.90987 19.8693 7.46102 20.1211 8.06783C20.3729 8.67465 20.5024 9.32519 20.5023 9.98217C20.5022 10.6391 20.3725 11.2896 20.1205 11.8964C19.8686 12.5031 19.4993 13.0542 19.034 13.518" stroke="black" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+            <path d="M13.103 19.516C12.154 20.4537 10.8737 20.9795 9.53952 20.9795C8.20539 20.9795 6.92502 20.4537 5.97602 19.516C5.50838 19.0536 5.13711 18.503 4.88373 17.8961C4.63035 17.2892 4.49988 16.6381 4.49988 15.9805C4.49988 15.3228 4.63035 14.6717 4.88373 14.0648C5.13711 13.4579 5.50838 12.9074 5.97602 12.445L6.50002 11.982" stroke="black" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+            <path d="M16.5 19.982H22.5" stroke="black" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+            <path d="M19.5 16.982V22.982" stroke="black" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+            </g>
+            <defs>
+            <clipPath id="clip0_41_4193">
+            <rect width="24" height="24" fill="white" transform="translate(0.5 0.981964)"/>
+            </clipPath>
+            </defs>
+        </svg>
+    )
+}
+
+export const FaucetIcon = () => {
+
+    return (
+        <svg width="100%" height="100%" viewBox="0 0 25 25" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <g clip-path="url(#clip0_41_4188)">
+            <path d="M8.00201 20.405C10.604 22.51 14.397 22.51 16.998 20.405C19.6 18.3 20.26 14.697 18.564 11.859L13.674 4.59897C13.254 3.97397 12.387 3.79597 11.738 4.20197C11.5751 4.30419 11.4354 4.43943 11.328 4.59897L6.43501 11.859C4.74001 14.697 5.40001 18.3 8.00201 20.405Z" stroke="black" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+            </g>
+            <defs>
+            <clipPath id="clip0_41_4188">
+            <rect width="24" height="24" fill="white" transform="translate(0.5 0.981964)"/>
+            </clipPath>
+            </defs>
+        </svg>
+    )
+}
+
+export const CloseIcon = () => {
+
+    return (
+        <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <g clipPath="url(#clip0_322_642)">
+            <mask id="mask0_322_642" style={{maskType:'luminance'}} maskUnits="userSpaceOnUse" x="0" y="0" width="24" height="24">
+            <path d="M24 0H0V24H24V0Z" fill="white"/>
+            </mask>
+            <g mask="url(#mask0_322_642)">
+            <path d="M7 7L17 17M7 17L17 7" stroke="#292929" stroke-width="2.5" stroke-linecap="round" stroke-linejoin="round"/>
+            </g>
+            </g>
+            <defs>
+            <clipPath id="clip0_322_642">
+            <rect width="24" height="24" fill="white"/>
+            </clipPath>
+            </defs>
+        </svg>
+    )
+}
